b5241b0a8d7503fd83b43d2f016495a2ad1aca1a
jt <cjt807916@gmail.com>
2022-01-28 15:14:49 +0800

    add an install.sh

---
+++ copyout_file.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cp ./shell/.bashrc ~/.bashrc
+cp ./shell/.tmux.conf ~/.tmux.conf 
+cp ./youtube-viewer/youtube-viewer.conf ~/.config/youtube-viewer/youtube-viewer.conf 
+cp ./shell/.zshrc ~/.zshrc 
+cp ./i3/.Xresources ~/.Xresources 
+cp ./alacritty/.alacritty.yml ~/.alacritty.yml 
+
+cp ./rofi/config.rasi ~/.config/rofi/config.rasi 
+cp ./picom/picom.conf ~/.config/picom.conf 
+cp ./i3/config ~/.i3/config 
+
